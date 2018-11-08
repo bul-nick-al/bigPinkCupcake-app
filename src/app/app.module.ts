e24bf5a63ea6055e7b838e3b4af5172a9ea3628d
nicholas <bul-nick-al@ya.ru>
2018-11-08 18:40:07 +0300

    bla

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { RouterModule } from '@angular/router';
+import { AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    RouterModule,
+    AmplifyAngularModule,
   ],
-  providers: [],
+  providers: [AmplifyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+export class PizzaPartyAppModule {}
+
+
---
+++ src/app/app.routing.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import {SearchComponent} from './components/search/search.component';
+// import { ParcelsPageComponent } from './pages/parsels-page/parsels-page.component';
+// import { AuthGuardService } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'index', component: SearchComponent},
+  // { path: 'login', component: LoginPageComponent }
+];
+
+@NgModule({
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  exports: []
+})
+export class AppRoutingModule {}
