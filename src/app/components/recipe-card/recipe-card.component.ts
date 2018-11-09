8355b04d8f276b2a8442cea39cc18676e95e6cf7
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-11-09 16:33:37 +0300

    Fix recipe card layout.

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeCardComponent implements OnInit {
 
+  public descriprion: string = 'These Mexican fritters are very common at fairs. In my border hometown, the line at this stand is always\n'+
+    '      overwhelming. People wait hours in line just to get a taste of these churros. I have run across several recipes\n' +
+    '      but this is the best one by far.';
+
   constructor() { }
 
   ngOnInit() {
