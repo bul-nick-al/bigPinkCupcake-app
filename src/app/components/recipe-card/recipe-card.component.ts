40a042d253ca96d4e5e7a513ef89ce532bce2d52
Georgiy Dzesov <g.dzesov@gmail.com>
2018-11-10 13:04:25 +0300

    accept recipe interface for card/recipe-full

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,19 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Recipe } from '../../interfaces/recipe';
 
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.css']
 })
-export class RecipeCardComponent implements OnInit {
-
-  public descriprion: string = 'These Mexican fritters are very common at fairs. In my border hometown, the line at this stand is always\n'+
-    '      overwhelming. People wait hours in line just to get a taste of these churros. I have run across several recipes\n' +
-    '      but this is the best one by far.';
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class RecipeCardComponent {
+  @Input()
+  recipe: Recipe;
 }
--- src/app/components/recipe-full/recipe-full.component.ts
+++ src/app/components/recipe-full/recipe-full.component.ts
@@ -1,25 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Recipe } from '../../interfaces/recipe';
 
 @Component({
   selector: 'app-recipe-full',
   templateUrl: './recipe-full.component.html',
   styleUrls: ['./recipe-full.component.css']
 })
-export class RecipeFullComponent implements OnInit {
-
+export class RecipeFullComponent {
   @Input()
-  peremennaya;
+  recipe: Recipe;
   @Output()
   backClick = new EventEmitter<void>();
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
 
   public onBackClick(): void {
     this.backClick.emit();
   }
-
 }
