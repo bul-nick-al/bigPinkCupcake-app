7afc1cb4527c1cd1adf989f1cdeedc520e923913
Georgiy Dzesov <g.dzesov@gmail.com>
2018-11-10 14:25:25 +0300

    accept recipe interface for card/recipe-full

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,12 +1,12 @@
-import {COMMA, ENTER} from '@angular/cdk/keycodes';
-import {Component, ElementRef, ViewChild} from '@angular/core';
-import {FormControl} from '@angular/forms';
-import {MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete} from '@angular/material';
-import {fromEvent, Observable} from 'rxjs';
-import {map, startWith} from 'rxjs/operators';
-import {debounceTime} from 'rxjs/internal/operators';
-import {PredictIngredientService} from '../../services/predict-ingredient.service';
-import {RecipeService} from '../../services/recipe.service';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatAutocompleteSelectedEvent, MatChipInputEvent, MatAutocomplete } from '@angular/material';
+import { fromEvent, Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { debounceTime } from 'rxjs/internal/operators';
+import { PredictIngredientService } from '../../services/predict-ingredient.service';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-search',
@@ -25,17 +25,25 @@ export class SearchComponent {
   ingredients: string[] = [];
   allIngredients: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
 
-  @ViewChild('ingredientInput') ingredientInput: ElementRef<HTMLInputElement>;
-  @ViewChild('auto') matAutocomplete: MatAutocomplete;
+  @ViewChild('ingredientInput')
+  ingredientInput: ElementRef<HTMLInputElement>;
+  @ViewChild('auto')
+  matAutocomplete: MatAutocomplete;
 
   constructor(private predictIngredientService: PredictIngredientService, private recipeService: RecipeService) {
-    this.ingredientCtrl.valueChanges.pipe(debounceTime(1000))
-      .subscribe((ingredient: string) => this.filteredIngredients = predictIngredientService.predictIngredient(ingredient));
+    this.ingredientCtrl.valueChanges
+      .pipe(debounceTime(1000))
+      .subscribe(
+        (ingredient: string) => (this.filteredIngredients = predictIngredientService.predictIngredient(ingredient))
+      );
   }
 
   add(event: MatChipInputEvent): void {
     // Add ingredient only when MatAutocomplete is not open
     // To make sure this does not conflict with OptionSelected Event
+    if (!this.ingredients.includes(event.value)) {
+      return;
+    }
     if (!this.matAutocomplete.isOpen) {
       const input = event.input;
       const value = event.value;
@@ -49,7 +57,7 @@ export class SearchComponent {
         input.value = '';
       }
       this.ingredientCtrl.setValue(null);
-      }
+    }
   }
 
   remove(ingredient: string): void {
@@ -77,6 +85,4 @@ export class SearchComponent {
 
     return this.allIngredients.filter(ingredient => ingredient.toLowerCase().indexOf(filterValue) === 0);
   }
-
 }
-
