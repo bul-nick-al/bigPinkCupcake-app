c7afd09586463f5e9a1c7a7411164aa946e405ff
AnastasiaVorton <anastasia.vorton@gmail.com>
2018-11-12 11:26:21 +0300

    Implement user sign up.

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../../services/auth.service';
-import {FormControl, FormGroup} from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-login-page',
@@ -10,14 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent implements OnInit {
-
   public formGroup: FormGroup;
+  public signUpClicked = new BehaviorSubject(false);
+  public registerButtonShown = new BehaviorSubject(false);
+  public signInOpened = new BehaviorSubject(true);
+  public confirmationFieldShown = new BehaviorSubject(false);
 
-  constructor(private authService: AuthService,  private router: Router) {
+  constructor(private authService: AuthService, private router: Router) {
     this.authService = authService;
     this.formGroup = new FormGroup({
       login: new FormControl(''),
       password: new FormControl(''),
+      email: new FormControl(''),
+      confirmationCode: new FormControl('')
     });
   }
 
@@ -26,13 +31,28 @@ export class LoginPageComponent implements OnInit {
   }
 
   onLoginClick() {
-    this.authService.signIn(this.formGroup.get('login').value, this.formGroup.get('password').value)
-      .subscribe(
-        () => this.router.navigate(['index']),
-        (error) => {
-          console.log(error);
-        }
-      );
+    this.authService.signIn(this.formGroup.get('login').value, this.formGroup.get('password').value).subscribe(
+      () => this.router.navigate(['index']),
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  onSignUpClick() {
+    this.authService.register(
+      this.formGroup.get('login').value,
+      this.formGroup.get('password').value,
+      this.formGroup.get('email').value
+    );
+    this.confirmationFieldShown.next(true);
+    this.registerButtonShown.next(false);
+  }
+
+  onConfirmClick() {
+    this.authService.confirm(this.formGroup.get('login').value, this.formGroup.get('confirmationCode').value);
+    this.signInOpened.next(true);
+    this.signUpClicked.next(false);
   }
 
   private checkIfAuthenticated(): void {
@@ -41,4 +61,9 @@ export class LoginPageComponent implements OnInit {
     }
   }
 
+  public onLinkClick() {
+    this.signInOpened.next(false);
+    this.signUpClicked.next(true);
+    this.registerButtonShown.next(true);
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-// import { AmplifyService } from 'aws-amplify-angular';
 import { AmplifyService } from 'aws-amplify-angular';
-import {fromPromise} from 'rxjs/internal/observable/fromPromise';
-import {Observable} from 'rxjs';
-import {CognitoUser} from 'amazon-cognito-identity-js';
-import {Router} from '@angular/router';
-import {map} from 'rxjs/internal/operators';
+import { fromPromise } from 'rxjs/internal/observable/fromPromise';
+import { Observable } from 'rxjs';
+import { CognitoUser } from 'amazon-cognito-identity-js';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/internal/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,12 +14,11 @@ export class AuthService {
 
   constructor(private amplifyService: AmplifyService, private router: Router) {
     this.user = null;
-    this.amplifyService.authStateChange$
-      .subscribe(authState => {
-        if (authState.state === 'signedIn') {
-          localStorage.setItem('isSignedIn', 'true');
-        }
-      });
+    this.amplifyService.authStateChange$.subscribe(authState => {
+      if (authState.state === 'signedIn') {
+        localStorage.setItem('isSignedIn', 'true');
+      }
+    });
   }
 
   public isSignedIn(): boolean {
@@ -28,7 +26,9 @@ export class AuthService {
   }
 
   public getUser(): Observable<string> {
-    return fromPromise(this.amplifyService.auth().currentAuthenticatedUser()).pipe(map(value => value.attributes.email));
+    return fromPromise(this.amplifyService.auth().currentAuthenticatedUser()).pipe(
+      map(value => value.attributes.email)
+    );
   }
 
   public signIn(login: string, password: string): Observable<CognitoUser> {
@@ -40,5 +40,13 @@ export class AuthService {
     localStorage.removeItem('isSignedIn');
     this.router.navigate(['login']);
     return fromPromise(this.amplifyService.auth().signOut());
+  }
+
+  public register(login, password, email): Observable<any> {
+    return fromPromise(this.amplifyService.auth().signUp(login, password, email));
+  }
+
+  public confirm(login, code): Observable<any> {
+    return fromPromise(this.amplifyService.auth().confirmSignUp(login, code));
   }
 }
