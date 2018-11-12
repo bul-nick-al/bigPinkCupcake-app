b974838e092f4cb3b15c8c6bf9e5fa56367f50eb
nicholas <bul-nick-al@ya.ru>
2018-11-13 00:26:30 +0300

    x

---
+++ src/app/services/email.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import AWS from '@aws-amplify/core';
+import {AmplifyService} from 'aws-amplify-angular';
+import {switchMap} from 'rxjs/operators';
+import {AuthService} from './auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class EmailService {
+  constructor(private amplifyService: AmplifyService, private auth: AuthService) {}
+
+  sendEmail(imageUrl: string, recipeName: string) {
+    this.auth
+      .getUserMore()
+      .pipe(
+        switchMap((user) =>
+          this.amplifyService
+            .api()
+            .get('bigPinkCupcake', `/send-email?name=
+            ${user.name}&recipe_name=${recipeName}&email=${user.email}&image_url=${imageUrl}`, null)
+        )
+      )
+      .subscribe(value => console.warn(value), error1 => console.warn(error1));
+  }
+}
